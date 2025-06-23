9e69e85db2e8887ee38016d96c5107c4e7c4f0bb
Vishal <85471038+vishalshetty104@users.noreply.github.com>
2025-06-23 15:02:04 +0530

    Update streamlit_app.py
    Added filtering to smoothiefroot dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ ingredients_str=''
 if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_str+=fruit_chosen+' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             # values ('""" + ingredients_str ++ ','+name_on_smoothie+"""')"""
